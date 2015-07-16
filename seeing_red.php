caa19baadb60965a2e1b318d0d7436a8fb6510c8
kjodle <kjodle@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-07-16 01:44:01 +0000

    Committing trunk 2.0 version of Seeing Red
    git-svn-id: https://plugins.svn.wordpress.org/seeing-red/trunk@1199742 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- seeing_red.php
+++ seeing_red.php
@@ -3,9 +3,10 @@
 Plugin Name: Seeing Red
 Plugin URI: http://www.kjodle.net/wordpress/seeing-red/
 Description: Makes "Screen Options" more noticeable
-Version: 1.1
+Version: 2.0
 Author: Kenneth John Odle
-Author URI: http://blog.kjodle.net
+Author URI: http://techblog.kjodle.net
+Text Domain: seeing-red
 
 Copyright 2015 Kenneth John Odle
 © 2015 Kenneth John Odle
@@ -29,19 +30,173 @@ Released under the GPL v.3, http://www.gnu.org/copyleft/gpl.html
 // Prevent this page from being loaded directly.
 defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
 
+
 //	Only use this if the current user can edit pages and posts
 function seeing_red_check() {
-   if ( ! current_user_can('edit_posts') && ! current_user_can('edit_pages') )
-     return;
+	if ( ! current_user_can('edit_posts') || ! current_user_can('edit_pages') )
+		return;
 }
 
-// Let's add our stylesheets:
-function d12_sr_styles() {
-	wp_register_style( 'd12sr_admin_css', plugins_url('css/red.css', __FILE__), false, '1.0.0' );
-	wp_enqueue_style( 'd12sr_admin_css' );
-}
-add_action( 'admin_enqueue_scripts', 'd12_sr_styles' );
 
-//	Color gradients generated at http://www.colorzilla.com/gradient-editor/
+// Register our activation/deactivation hooks
+// register_activation_hook(__FILE__, 'add_defaults_d12sr');
+register_uninstall_hook( __FILE__, 'delete_defaults_d12sr' );
+
+// Define default option settings
+/* Keep this until we get it figured out (or determine if it's even necessary).
+function add_defaults_d12sr(){
+	$tmp = get_option('d12sr_options');
+	if(!is_array($tmp)) {
+		$arr = array(
+			"so" => "Red",
+			"ch" => "Red"
+		);
+	}
+	update_option('d12sr_options', $arr);
+}
+*/
+
+// Delete database settings upon uninstall
+function delete_defaults_d12sr(){
+	$option_name = 'd12sr_options';
+	delete_option( $option_name );
+}
+
+
+// Create a new admin panel
+function d12sr_admin_panel_setup(){
+	add_submenu_page(
+	'options-general.php',
+	'Seeing Red Options',
+	'Seeing Red',
+	'manage_options',
+	'seeing-red-options',
+	'd12sr_options_callback'
+	);
+}
+add_action('admin_menu', 'd12sr_admin_panel_setup');
+
+
+// Callback to create the setting page
+function d12sr_options_callback(){
+?>
+	<div class="wrap">
+	<h2>Seeing Red Options</h2>
+	<p><strong>New!</strong> You now have the option to style the "Screen Options" and "Help" tabs separately.</p>
+	<p>You can also choose from other color schemes besides red.</p>
+	<form method="post" action="options.php"> 
+	<?php settings_fields( 'd12sr_options_group' ); ?>
+	<?php do_settings_sections( 'd12sr_settings' ); ?>
+	<input name="Submit" type="submit" class="button-primary" value="<?php esc_attr_e(__('Save Options','seeing-red')); ?>" />
+	</form>
+	</div>
+<?php
+};
+
+// Add the admin settings
+add_action( 'admin_init', 'd12sr_admin_init');
+function d12sr_admin_init() {
+	register_setting(
+		'd12sr_options_group',
+		'd12sr_options',
+		'd12sr_options_validate'
+	);
+	add_settings_section(
+		'd12sr_main',
+		'',
+		'so_settings_section',
+		'd12sr_settings'
+	);
+	add_settings_field(
+		'd12sr_so',
+		'Screen Options tab color',
+		'so_settings_field',
+		'd12sr_settings',
+		'd12sr_main'
+	);
+	add_settings_field(
+		'd12sr_ch',
+		'Help tab color',
+		'ch_settings_field',
+		'd12sr_settings',
+		'd12sr_main'
+	);
+};
+
+// Callback for setting_section
+function so_settings_section() {
+	echo '';
+};
+
+// Callback for settings_field for Screen Options tab
+function so_settings_field(){
+	$options = get_option( 'd12sr_options' );
+	$items = array("Red", "Green", "Blue", "Default");
+	echo "<select id='d12sr-so' name='d12sr_options[so]'>";
+	foreach($items as $item) {
+		$selected = ($options[so]==$item) ? 'selected="selected"' : '';
+		echo "<option value='$item' $selected>$item</option>";
+	}
+	echo "</select>";
+};
+
+// Callback for settings_field for Contextual Help tab
+function ch_settings_field(){
+	$options = get_option( 'd12sr_options' );
+	$items = array("Red", "Green", "Blue", "Default");
+	echo "<select id='d12sr-ch' name='d12sr_options[ch]'>";
+	foreach($items as $item) {
+		$selected = ($options[ch]==$item) ? 'selected="selected"' : '';
+		echo "<option value='$item' $selected>$item</option>";
+	}
+	echo "</select>";
+};
+
+// Since we are only using drop-downs, we don't need to validate
+function d12sr_options_validate($input) {
+	return $input; 
+}
+
+
+// Retrieve Screen Options options from database, register appropriate stylesheet, and enqueue
+function d12so_retrieve() {
+	$sroptions = get_option('d12sr_options');
+	switch($sroptions['so']) {
+		case "Red" :
+			wp_register_style( 'd12sr_admin_socss', plugins_url('css/sored.css', __FILE__), false, '1.0.0' );
+			break;
+		case "Green" :
+			wp_register_style( 'd12sr_admin_socss', plugins_url('css/sogreen.css', __FILE__), false, '1.0.0' );
+			break;
+		case "Blue" :
+			wp_register_style( 'd12sr_admin_socss', plugins_url('css/soblue.css', __FILE__), false, '1.0.0' );
+			break;
+		default:
+			return;
+	}
+	wp_enqueue_style( 'd12sr_admin_socss' );
+}
+add_action( 'admin_enqueue_scripts', 'd12so_retrieve' );
+
+
+// Retrieve Screen Options options from database, register appropriate stylesheet, and enqueue
+function d12ch_retrieve() {
+	$sroptions = get_option('d12sr_options');
+	switch($sroptions['ch']) {
+		case "Red" :
+			wp_register_style( 'd12sr_admin_chcss', plugins_url('css/chred.css', __FILE__), false, '1.0.0' );
+			break;
+		case "Green" :
+			wp_register_style( 'd12sr_admin_chcss', plugins_url('css/chgreen.css', __FILE__), false, '1.0.0' );
+			break;
+		case "Blue" :
+			wp_register_style( 'd12sr_admin_chcss', plugins_url('css/chblue.css', __FILE__), false, '1.0.0' );
+			break;
+		default:
+			return;
+	}
+	wp_enqueue_style( 'd12sr_admin_chcss' );
+}
+add_action( 'admin_enqueue_scripts', 'd12ch_retrieve' );
 
 ?>
